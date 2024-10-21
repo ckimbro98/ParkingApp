8aab7e36544437401e7fc8157ab3e2f7d499daa4
Andrew Knox <andrew.knox.r@gmail.com>
2024-10-20 22:29:11 -0700

    adding a simple button

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -10,6 +10,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button title="Use Location" color="red"></Button>
     </View>
   );
 }
