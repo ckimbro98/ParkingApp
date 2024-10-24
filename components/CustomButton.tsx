861646866a988e1c37c8d95947408f8cc148680e
Andrew Knox <andrew.knox.r@gmail.com>
2024-10-24 13:07:04 -0700

    adding custom button component

---
+++ components/CustomButton.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Pressable, Text, StyleSheet } from 'react-native';
+
+const CustomButton = ({
+  onPress,
+  title,
+}: {
+  onPress: () => void;
+  title: string;
+}) => {
+  return (
+    <Pressable onPress={onPress} style={styles.button}>
+      <Text style={styles.text}>{title}</Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'blue',
+    borderRadius: 25, // For rounded corners
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default CustomButton;
