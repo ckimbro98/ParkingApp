350e47eddadd95739c4abdec9f5bcbf3d1b3d49f
Andrew Knox <andrew.knox.r@gmail.com>
2024-10-24 13:07:14 -0700

    adding home page view

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,69 @@
-import { Button, Text, View } from "react-native";
+import { StyleSheet, Button, Text, View } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import CustomButton from '@/components/CustomButton';
 
 export default function Index() {
+  const browseLotsHandler = () => {
+    console.log('browseLotsHandler');
+  };
+
+  const useMyLocationHandler = () => {
+    console.log('useMyLocationHandler');
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Button title="Use Location" color="red"></Button>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Text style={styles.title}>Where are you parking?</Text>
+        </View>
+        <View>
+          <CustomButton
+            onPress={useMyLocationHandler}
+            title="Use My Location"
+          />
+
+          <View style={styles.separator}></View>
+          <View style={styles.button}>
+            <Button
+              title="Browse parking lots"
+              color={'white'}
+              onPress={browseLotsHandler}
+            ></Button>
+          </View>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'stretch',
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+    fontSize: 24,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  button: {
+    backgroundColor: 'blue',
+    borderRadius: 25, // For rounded corners
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginHorizontal: 30,
+    alignItems: 'center',
+  },
+});
