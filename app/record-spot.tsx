d83eeaf148ef5dbc21be97b0ed49580762b641aa
Andrew Knox <andrew.knox.r@gmail.com>
2024-10-25 14:57:37 -0700

    adding a screen to display the location details

---
+++ app/record-spot.tsx
@@ -0,0 +1,103 @@
+import { View, Text, StyleSheet } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import { useState, useEffect } from 'react';
+import CustomButton from '@/components/CustomButton';
+
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+import * as Location from 'expo-location';
+import { useLocalSearchParams } from 'expo-router';
+
+export default function RecordSpot() {
+  const colorScheme = useColorScheme();
+
+  const location = useLocalSearchParams();
+  const parsedLocation = JSON.parse(
+    location?.location as string,
+  ) as Location.LocationObject;
+  const coords = parsedLocation.coords;
+  const timestamp = parsedLocation.timestamp;
+
+  useEffect(() => {
+    console.log('location', location);
+    console.log(coords);
+    console.log(timestamp);
+  }, [location]);
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={{ alignItems: 'center' }}>
+          <Text
+            style={{ color: Colors[colorScheme ?? 'light'].text, fontSize: 24 }}
+          >
+            Recorded Location
+          </Text>
+          <View style={styles.columnContainer}>
+            <View style={styles.column}>
+              <Text style={{ color: Colors[colorScheme ?? 'light'].text }}>
+                Latitude
+              </Text>
+              <Text style={{ color: Colors[colorScheme ?? 'light'].text }}>
+                Longitude
+              </Text>
+              <Text style={{ color: Colors[colorScheme ?? 'light'].text }}>
+                Altitude
+              </Text>
+              <Text style={{ color: Colors[colorScheme ?? 'light'].text }}>
+                Altitude Accuracy
+              </Text>
+              <Text style={{ color: Colors[colorScheme ?? 'light'].text }}>
+                Heading
+              </Text>
+              <Text style={{ color: Colors[colorScheme ?? 'light'].text }}>
+                Speed
+              </Text>
+            </View>
+            <View style={styles.column}>
+              <Text style={{ color: Colors[colorScheme ?? 'light'].text }}>
+                {coords.latitude}
+              </Text>
+              <Text style={{ color: Colors[colorScheme ?? 'light'].text }}>
+                {coords.longitude}
+              </Text>
+              <Text style={{ color: Colors[colorScheme ?? 'light'].text }}>
+                {coords.altitude}
+              </Text>
+              <Text style={{ color: Colors[colorScheme ?? 'light'].text }}>
+                {coords.altitudeAccuracy}
+              </Text>
+              <Text style={{ color: Colors[colorScheme ?? 'light'].text }}>
+                {coords.heading}
+              </Text>
+              <Text style={{ color: Colors[colorScheme ?? 'light'].text }}>
+                {coords.speed}
+              </Text>
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'stretch',
+    marginHorizontal: 16,
+  },
+  columnContainer: {
+    flexDirection: 'row', // Row layout for side-by-side columns
+    justifyContent: 'space-between', // Space between the columns
+    width: '100%',
+    marginTop: 20,
+  },
+  column: {
+    flex: 1, // Each column takes equal width
+    alignItems: 'center', // Center content horizontally
+    padding: 10,
+  },
+});
