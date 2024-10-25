cf561cb2ea603b6e18797a3e7701b1fa1e1ce5a0
Andrew Knox <andrew.knox.r@gmail.com>
2024-10-25 14:57:54 -0700

    allowing custom button to be used with Link via forwardRef

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,34 +1,38 @@
 import React from 'react';
 import { View, Pressable, Text, StyleSheet } from 'react-native';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
 
-const CustomButton = ({
-  onPress,
-  title,
-}: {
+interface CustomButtonProps {
   onPress: () => void;
   title: string;
-}) => {
+}
+
+const CustomButton = React.forwardRef<View, CustomButtonProps>((props, ref) => {
+  const { onPress, title } = props;
+  const colorScheme = useColorScheme();
+
+  const styles = StyleSheet.create({
+    button: {
+      backgroundColor: Colors[colorScheme ?? 'light'].background,
+      borderRadius: 25, // For rounded corners
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      marginHorizontal: 30,
+      alignItems: 'center',
+    },
+    text: {
+      color: Colors[colorScheme ?? 'light'].text,
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
+  });
+
   return (
-    <Pressable onPress={onPress} style={styles.button}>
+    <Pressable ref={ref} onPress={onPress} style={styles.button}>
       <Text style={styles.text}>{title}</Text>
     </Pressable>
   );
-};
-
-const styles = StyleSheet.create({
-  button: {
-    backgroundColor: 'blue',
-    borderRadius: 25, // For rounded corners
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    marginHorizontal: 30,
-    alignItems: 'center',
-  },
-  text: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
 });
 
 export default CustomButton;
