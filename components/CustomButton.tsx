e4cc9c8776b7068c545f3c6c7a20ffea7058d115
Andrew Knox <andrew.knox.r@gmail.com>
2024-10-29 15:25:51 -0700

    styling

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -18,7 +18,6 @@ const CustomButton = React.forwardRef<View, CustomButtonProps>((props, ref) => {
       borderRadius: 25, // For rounded corners
       paddingVertical: 10,
       paddingHorizontal: 20,
-      marginHorizontal: 30,
       alignItems: 'center',
     },
     text: {
