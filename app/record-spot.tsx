2337652c157d42f65ce09bd2181999b505eaf6fb
Andrew Knox <andrew.knox.r@gmail.com>
2024-10-28 17:03:35 -0700

    adding adjustable map component to the record-spot icon

--- app/record-spot.tsx
+++ app/record-spot.tsx
@@ -2,6 +2,7 @@ import { View, Text, StyleSheet } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { useState, useEffect } from 'react';
 import CustomButton from '@/components/CustomButton';
+import AdjustableMap from '@/components/AdjustableMap';
 
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
@@ -34,7 +35,44 @@ export default function RecordSpot() {
           >
             Recorded Location
           </Text>
-          <View style={styles.columnContainer}>
+          <AdjustableMap
+            initialRegion={{
+              latitude: coords.latitude,
+              longitude: coords.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          ></AdjustableMap>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'stretch',
+    marginHorizontal: 16,
+  },
+  columnContainer: {
+    flexDirection: 'row', // Row layout for side-by-side columns
+    justifyContent: 'space-between', // Space between the columns
+    width: '100%',
+    marginTop: 20,
+  },
+  column: {
+    flex: 1, // Each column takes equal width
+    alignItems: 'center', // Center content horizontally
+    padding: 10,
+  },
+});
+
+/**
+ *  Old code that tells us what variables we have access to with currentLocation
+ * 
+ * <View style={styles.columnContainer}>
             <View style={styles.column}>
               <Text style={{ color: Colors[colorScheme ?? 'light'].text }}>
                 Latitude
@@ -76,28 +114,4 @@ export default function RecordSpot() {
               </Text>
             </View>
           </View>
-        </View>
-      </SafeAreaView>
-    </SafeAreaProvider>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'stretch',
-    marginHorizontal: 16,
-  },
-  columnContainer: {
-    flexDirection: 'row', // Row layout for side-by-side columns
-    justifyContent: 'space-between', // Space between the columns
-    width: '100%',
-    marginTop: 20,
-  },
-  column: {
-    flex: 1, // Each column takes equal width
-    alignItems: 'center', // Center content horizontally
-    padding: 10,
-  },
-});
+ */
