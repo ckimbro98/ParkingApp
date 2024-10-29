917446cd932019475c70b0498255659a846a6d68
Andrew Knox <andrew.knox.r@gmail.com>
2024-10-29 11:47:23 -0700

    adding a hook to save locations

---
+++ hooks/useLocations.ts
@@ -0,0 +1,59 @@
+// useLocations.ts
+import { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const STORAGE_KEY = 'locations';
+
+const useLocations = () => {
+  const [locations, setLocations] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // Add a location to local storage
+  const addLocation = async (location: {
+    latitude: number;
+    longitude: number;
+  }) => {
+    try {
+      const storedLocations = await AsyncStorage.getItem(STORAGE_KEY);
+      const newLocations = storedLocations ? JSON.parse(storedLocations) : [];
+      newLocations.push(location);
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(newLocations));
+      setLocations(newLocations);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // Remove a location from local storage
+  const removeLocation = async (index: number) => {
+    try {
+      const storedLocations = await AsyncStorage.getItem(STORAGE_KEY);
+      const newLocations = storedLocations ? JSON.parse(storedLocations) : [];
+      newLocations.splice(index, 1);
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(newLocations));
+      setLocations(newLocations);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // Get locations from local storage
+  const getLocations = async () => {
+    try {
+      const storedLocations = await AsyncStorage.getItem(STORAGE_KEY);
+      const newLocations = storedLocations ? JSON.parse(storedLocations) : [];
+      setLocations(newLocations);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getLocations();
+    setLoading(false);
+  }, []);
+
+  return { locations, addLocation, removeLocation, loading };
+};
+
+export default useLocations;
